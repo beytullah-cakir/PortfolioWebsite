fdc41c0c15420de7d3717076e9d98533612bb321
Beytullah Çakır <132066307+beytullah-cakir@users.noreply.github.com>
2025-12-30 20:02:05 +0300

    fixed update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,7 +10,10 @@ namespace Portfolio.Controllers
         {
             return View();
         }
-        
-        
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
